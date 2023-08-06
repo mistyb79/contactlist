c4f9e12aaff089b17a03bbe31368c76eb6fe5b34
Misty <mistybarnes79@gmail.com>
2023-08-06 15:23:41 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, {useState, useEffect} from "react";
+import ContactList from "./components/ContactList";
 
+//const dummyContacts = [
+ // { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  //{ id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  //{id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+//];
+
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  console.log("Contacts:", contacts);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error("There was an error fetching contacts", error);
+      }
+    };
+    fetchContacts();
+  }, []);
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error("There was an error fetching contacts", error);
+      }
+    };
+    fetchContacts();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    {selectedContactId ? (
+    <h1> Contact Selected</h1> 
+    ) : (
+      <ContactList contacts={contacts} setSelectedContactId={setSelectedContactId}/>
+  )}
+  </>
   )
 }
 
-export default App
+export default App; 
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ContactRow from "./ContactRow";
+
+const ContactList = ({ contacts, setSelectedContactId }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {contacts.map((contact) => {
+          return (
+            <ContactRow
+              key={contact.id}
+              contact={contact}
+              setSelectedContactId={setSelectedContactId}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default ContactList;
